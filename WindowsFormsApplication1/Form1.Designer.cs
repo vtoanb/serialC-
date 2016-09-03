d2d086a0dd5a07e174c60131ebca2bb078d20a59
vtoanb <vtoanb@gmail.com>
2016-09-03 10:11:54 +0700

    generated executable file

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.connect = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -46,40 +47,43 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(36, 310);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 118);
+            this.button1.Size = new System.Drawing.Size(101, 99);
             this.button1.TabIndex = 0;
-            this.button1.Text = "N1";
+            this.button1.Text = "L1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(192, 310);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(175, 310);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 118);
+            this.button2.Size = new System.Drawing.Size(101, 99);
             this.button2.TabIndex = 0;
-            this.button2.Text = "button1";
+            this.button2.Text = "L2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(348, 310);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(314, 310);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 118);
+            this.button3.Size = new System.Drawing.Size(101, 99);
             this.button3.TabIndex = 0;
-            this.button3.Text = "button1";
+            this.button3.Text = "L3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(504, 310);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(453, 310);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(138, 118);
+            this.button4.Size = new System.Drawing.Size(101, 99);
             this.button4.TabIndex = 0;
-            this.button4.Text = "button1";
+            this.button4.Text = "L4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // serialPort1
@@ -103,7 +107,7 @@
             this.label2.AutoSize = true;
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(31, 53);
+            this.label2.Location = new System.Drawing.Point(31, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 26);
             this.label2.TabIndex = 2;
@@ -113,10 +117,9 @@
             // comboBox1
             // 
             this.comboBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(36, 109);
+            this.comboBox1.Location = new System.Drawing.Point(36, 120);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(224, 33);
             this.comboBox1.TabIndex = 3;
@@ -125,7 +128,7 @@
             // 
             this.connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.connect.Location = new System.Drawing.Point(36, 163);
+            this.connect.Location = new System.Drawing.Point(303, 120);
             this.connect.Name = "connect";
             this.connect.Size = new System.Drawing.Size(224, 33);
             this.connect.TabIndex = 4;
@@ -133,19 +136,34 @@
             this.connect.UseVisualStyleBackColor = true;
             this.connect.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(592, 310);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 99);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "L5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 519);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(743, 506);
             this.Controls.Add(this.connect);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(759, 544);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -165,6 +183,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -15,6 +15,8 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -22,19 +24,73 @@ namespace WindowsFormsApplication1
             // Display available comport to commbo box
             string[] portname = SerialPort.GetPortNames();
             comboBox1.DataSource = portname;
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
            
             
         }
 
-        public void readUart()
+        private void DataReceivedHandler(
+                       object sender,
+                       SerialDataReceivedEventArgs e)
         {
-            while (true)
-            {
-                string dataline = serialPort1.ReadLine();
-                // Put read data to button text to test
-                SetText(dataline);
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadLine().Replace('\r','-');
+            string[] parsed = indata.Split('-');
+
+            // Set color for button according to received message
+            switch (parsed[0]) { 
+                case "L1":
+                    if (parsed[1] == "1")
+                    {
+                        button1.BackColor = Color.YellowGreen;
+                    }
+                    else if (parsed[1] == "0")
+                    {
+                        button1.BackColor = Color.White;
+                    }
+
+                    break;
+                case "L2":
+                    if (parsed[1] == "1")
+                    {
+                        button2.BackColor = Color.YellowGreen;
+                    }
+                    else if (parsed[1] == "0")
+                    {
+                        button2.BackColor = Color.White;
+                    }
+                    break;
+                case "L3":
+                    if (parsed[1] == "1")
+                    {
+                        button3.BackColor = Color.YellowGreen;
+                    }
+                    else if (parsed[1] == "0")
+                    {
+                        button3.BackColor = Color.White;
+                    }
+                    break;
+                case "L4":
+                    if (parsed[1] == "1")
+                    {
+                        button4.BackColor = Color.YellowGreen;
+                    }
+                    else if (parsed[1] == "0")
+                    {
+                        button4.BackColor = Color.White;
+                    }
+                    break;
+                case "L5":
+                    if (parsed[1] == "1")
+                    {
+                        button5.BackColor = Color.YellowGreen;
+                    }
+                    else if (parsed[1] == "0")
+                    {
+                        button5.BackColor = Color.White;
+                    }
+                    break;
             }
-           
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -60,15 +116,23 @@ namespace WindowsFormsApplication1
         private void button5_Click(object sender, EventArgs e)
         {
             // Start new thread to read uart
-            Thread serialReadThread = new Thread(new ThreadStart(readUart));
-            serialPort1.Close();
-            connect.Text = "CONNECTING...";
-            serialPort1.PortName = comboBox1.SelectedItem.ToString();
-            serialPort1.Open();
-            serialReadThread.Start();
-            connect.Text = "CONNECTED";
-            connect.BackColor = Color.Green;
+            if (connect.Text == "CONNECT" && comboBox1.SelectedItem.ToString().Contains("COM"))
+            {
+                serialPort1.Close();
+                connect.Text = "CONNECTING...";
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.Open();
+                connect.Text = "DISCONNECT";
+                connect.BackColor = Color.Green;
+            }
+            else if (connect.Text == "DISCONNECT") {
+                serialPort1.Close();
+                connect.Text = "CONNECT";
+                connect.BackColor = Color.White;
+            }
         }
+
+
 
         // Safe edit text for button
         delegate void SetTextCallback(string text);
